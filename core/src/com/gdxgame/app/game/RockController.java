e7d8f6c3b536b5fe23ce087ba5ed22d91a44c086
Serg Bogomolov <serg@bogomolov.su>
2022-03-04 17:09:59 +0300

    Lesson 2

--- core/src/com/gdxgame/app/game/RockController.java
+++ core/src/com/gdxgame/app/game/RockController.java
@@ -1,10 +1,2 @@
-package com.gdxgame.app;
-
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.gdxgame.app.helpers.ObjectPool;
-import com.gdxgame.app.screen.utils.Assets;
-
-public class RockController extends ObjectPool<Rock> {
-
+package com.gdxgame.app.game;public class RockController {
 }
